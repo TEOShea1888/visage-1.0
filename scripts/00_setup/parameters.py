251afdd092fbf174cef48f4abbd366779b79ad84
Thomas O'Shea <thomasoshea1988@gmail.com>
2026-03-12 11:42:09 +0000

    Create parameters.py

---
+++ scripts/00_setup/parameters.py
@@ -0,0 +1,14 @@
+"""
+parameters.py
+-------------
+Centralised model parameters for ViSAGE 1.0.
+"""
+
+# Distance decay parameter
+BETA = -0.15
+
+# Weight for greenspace quality attractor
+QUALITY_WEIGHT = 1.0
+
+# Maximum walking distance (metres)
+MAX_DISTANCE = 5000
