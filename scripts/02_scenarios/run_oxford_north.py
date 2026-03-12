c14e5c77d1d3550fdd7825ad09814b9d7f70b01b
Thomas O'Shea <thomasoshea1988@gmail.com>
2026-03-12 11:48:25 +0000

    Create run_oxford_north.py to run development scenario
    This script is designed to execute the Oxford North development scenario.

---
+++ scripts/02_scenarios/run_oxford_north.py
@@ -0,0 +1,9 @@
+"""
+run_oxford_north.py
+-------------------
+Runs the Oxford North development scenario.
+"""
+
+def run_oxford_north():
+    """Execute Oxford North scenario."""
+    print("Running Oxford North scenario...")
